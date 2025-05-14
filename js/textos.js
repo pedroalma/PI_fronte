7aafd929683fe1f82d53d517cf8d3c1c28b74aa7
pedroalma <pedroalma554@gmail.com>
2025-05-14 11:48:33 -0300

    1.0.0

---
+++ js/textos.js
@@ -0,0 +1,22 @@
+// let  n1 
+// let  n2 
+// let test = 5
+// let fim
+
+// function calcula(){
+//     let n1 = document.getElementById("n1").value;
+
+//     let n2 = document.getElementById("n2").value;
+    
+//     // test = n1/n2;
+//     // fim = test*n2
+//     fim = test+5;
+// //console.log("test")
+// document.getElementById("fim").innerHTML=fim;
+// let texto1 , texto2;
+// let galeria;
+
+document.getElementById("h1j").innerHTML=`oal`;
+document.getElementById("imgj").innerHTML = `
+<img src="./img/image (1).png" alt="">
+`;
